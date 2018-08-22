60d1975a33a24d9c86cf1c90405cc70886b6e8f0
Marco Bruno <marco.bruno.br@gmail.com>
2018-08-22 03:29:00 +0000

    Fim da nona aula

--- projeto/js/component/main-expand.js
+++ projeto/js/component/main-expand.js
@@ -1,8 +1,15 @@
 var mainExpand = window.document.querySelector(".main-expand");
+var mainExpandSubtitle = mainExpand.querySelector(".subtitle");
 var mainCard = window.document.querySelector(".main-card");
 
 function clickMainExpand() {
-  mainCard.classList.add("-isActive");
+  if (mainCard.classList.contains("-isActive")) {
+    mainCard.classList.remove("-isActive");
+    mainExpandSubtitle.textContent = "Ver grande";
+  } else {
+    mainCard.classList.add("-isActive");
+    mainExpandSubtitle.textContent = "Ver pequeno";
+  }
 }
 
 mainExpand.addEventListener("click", clickMainExpand);
