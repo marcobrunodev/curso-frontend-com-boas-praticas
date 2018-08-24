22874fdb57659f348bc71d2ddd72b952fd32e807
Marco Bruno <marco.bruno.br@gmail.com>
2018-08-24 01:00:31 +0000

    Feito o esquema do bullet sem o loop

--- projeto/js/component/main-carousel.js
+++ projeto/js/component/main-carousel.js
@@ -1,10 +1,9 @@
 /*
-  Objetivo: deixar o primeiro bullet com opacity 1 assim que o usuário clicar nele
+  Obejtivo: Clicar em qualquer um dos bullets, tirar o -isActive do bullet que estiver com ela
 
-  1 - Pegar o primeiro component com class bullet (OK)
-  2 - Adiconar um ouvinte para o evento de clique do component que pegarmos anteriormente (OK)
-  3 - No evento do clique nós queremos adicionar a class -isActive (OK)
-  4 - Se o component bullet tiver a class -isActive nos queremos removéla caso contrario nós deveremos adicionar a class -isActive
+  1 - Pegar o elemento bullet que está com a class -isActive
+  2 - Remove a class -isActive desse elemento quando clicar no primeiro bullet
+  3 - Remvoe a class -isActive desse elemento quando qualquer bullet sofrer o evento de click
 */
 
 /* 
@@ -12,14 +11,18 @@
   DOM (Document Object Model) - document
 */
 
-var $bullet = document.querySelector('.bullet')
+var $bullet = document.querySelectorAll('.bullet')
+var $bulletIsActive = document.querySelector('.bullet.-isActive')
 
-$bullet.addEventListener('click', clickInBullet)
+$bullet[0].addEventListener('click', clickInBullet)
+$bullet[1].addEventListener('click', clickInBullet)
+$bullet[2].addEventListener('click', clickInBullet)
 
 function clickInBullet() {
-  $bullet.classList.toggle('-isActive')
+  $bulletIsActive.classList.remove('-isActive')
+  this.classList.add('-isActive')
+  $bulletIsActive = this
 }
-
 
 
 
